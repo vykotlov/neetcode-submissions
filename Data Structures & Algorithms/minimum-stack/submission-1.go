b43bac48b428d34b654133a67ae75de2ecb4aec8
Vyacheslav Kotlov <106555814+vykotlov@users.noreply.github.com>
2026-07-05 21:01:02 +0300

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.go
@@ -0,0 +1,37 @@
+type MinStack struct {
+	items []int
+	mins  []int
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (s *MinStack) Push(val int) {
+	newMin := val
+
+	if len(s.items) > 0 {
+		currentMin := s.GetMin()
+		if currentMin < newMin {
+			newMin = currentMin
+		}
+	}
+
+	s.items = append(s.items, val)
+	s.mins = append(s.mins, newMin)
+}
+
+func (s *MinStack) Pop() {
+	n := len(s.items) - 1
+
+	s.items = s.items[:n]
+	s.mins = s.mins[:n]
+}
+
+func (s *MinStack) Top() int {
+	return s.items[len(s.items)-1]
+}
+
+func (s *MinStack) GetMin() int {
+	return s.mins[len(s.mins)-1]
+}
