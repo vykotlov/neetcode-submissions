f064318a44c4a159044b4af6ea728850375c459f
Vyacheslav Kotlov <106555814+vykotlov@users.noreply.github.com>
2026-07-06 15:34:35 +0300

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.go
@@ -0,0 +1,24 @@
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left < right {
+		mid := (left + right) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	if nums[left] == target {
+		return left
+	}
+
+	return -1
+}
