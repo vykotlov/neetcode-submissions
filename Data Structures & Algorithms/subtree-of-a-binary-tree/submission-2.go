2fd94244284149eb6142ed22c85f1873434735dd
Vyacheslav Kotlov <106555814+vykotlov@users.noreply.github.com>
2026-07-23 17:23:32 +0300

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.go
@@ -0,0 +1,21 @@
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+
+	return isSameTree(root, subRoot) || isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil || q == nil {
+		return false
+	}
+
+	return p.Val == q.Val &&
+		isSameTree(p.Left, q.Left) &&
+		isSameTree(p.Right, q.Right)
+}
