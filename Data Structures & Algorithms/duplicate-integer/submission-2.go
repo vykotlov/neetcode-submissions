3b0a4d3d287490ebc7b6af2ad1ac9513755cb9a8
Vyacheslav Kotlov <106555814+vykotlov@users.noreply.github.com>
2026-08-01 00:00:39 +0300

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.go
@@ -0,0 +1,14 @@
+func hasDuplicate(nums []int) bool {
+	unique := make(map[int]bool, len(nums))
+
+	for _, num := range nums {
+		if _, contains := unique[num]; contains {
+			return true
+		}
+
+		unique[num] = true
+	}
+
+
+	return false
+}
